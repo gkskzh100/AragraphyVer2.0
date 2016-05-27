79f17de96684a6e774878e05366790b36fd22d61
gkskzh100 <a22569447@gmail.com>
2016-05-27 10:02:11 +0900

    Kim ver 0.2
    activity_main.xml

--- app/src/main/java/jm/dodam/aragraphyver20/MainActivity.java
+++ app/src/main/java/jm/dodam/aragraphyver20/MainActivity.java
@@ -1,6 +1,7 @@
 package jm.dodam.aragraphyver20;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -10,6 +11,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        startActivity(new Intent(this, SplashActivity.class));
 
     }
 }
---
+++ app/src/main/java/jm/dodam/aragraphyver20/SplashActivity.java
@@ -0,0 +1,24 @@
+package jm.dodam.aragraphyver20;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.os.Message;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Handler handler = new Handler() {
+            @Override
+            public void handleMessage(Message msg) {
+                finish();
+            }
+        };
+
+        handler.sendEmptyMessageDelayed(0, 3000);
+    }
+}
