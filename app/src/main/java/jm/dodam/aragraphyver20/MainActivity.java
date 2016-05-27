7cc01c2d03a437ade1aabd36de5c623e6b54fffb
gkskzh100 <a22569447@gmail.com>
2016-05-27 09:38:25 +0900

    Kim

--- app/src/main/java/jm/dodam/aragraphyver20/MainActivity.java
+++ app/src/main/java/jm/dodam/aragraphyver20/MainActivity.java
@@ -1,15 +1,15 @@
 package jm.dodam.aragraphyver20;
 
-import android.app.Activity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 
-public class MainActivity extends Activity {
+public class MainActivity extends FragmentActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
 
     }
 }
