985fa44644ca320c4569b1ae6a24337974c1adbe
bhy98528 <bhy98528@gmail.com>
2016-06-02 09:16:12 +0900

    Bong ver 1.3
    MainActivity/activity_main

--- app/src/main/java/jm/dodam/aragraphyver20/MainActivity.java
+++ app/src/main/java/jm/dodam/aragraphyver20/MainActivity.java
@@ -7,11 +7,11 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import com.astuetz.PagerSlidingTabStrip;
 
 public class MainActivity extends AppCompatActivity {
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +27,10 @@ public class MainActivity extends AppCompatActivity {
 
         PagerSlidingTabStrip tabStrip = (PagerSlidingTabStrip) findViewById(R.id.mainTabBar);
         tabStrip.setViewPager(viewPager);
+
+        LinearLayout linearLayout = (LinearLayout) findViewById(R.id.menuLayout);
+        linearLayout.bringToFront();
+        linearLayout.invalidate();
 
 
     }
