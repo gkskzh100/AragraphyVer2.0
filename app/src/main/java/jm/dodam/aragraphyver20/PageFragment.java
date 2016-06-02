6856313d8cc3e73c27885834daecff74fee110b3
gkskzh100 <a22569447@gmail.com>
2016-06-02 09:51:31 +0900

    Kim ver 1.4
    timeLine.xml in LikeBtn

--- app/src/main/java/jm/dodam/aragraphyver20/PageFragment.java
+++ app/src/main/java/jm/dodam/aragraphyver20/PageFragment.java
@@ -1,5 +1,6 @@
 package jm.dodam.aragraphyver20;
 
+import android.media.Image;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,16 +9,27 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class PageFragment extends Fragment {
+    private int i;
+
     public static final String ARG_PAGE = "ARG_PAGE";
     private RecyclerView recyclerView;
     private int pageNum;
     private List<RecyclerItem> items = new ArrayList<>();
     private RecyclerItem item[] = new RecyclerItem[10];
+
+    private ImageButton timeLineLikeBtn;
+    private TextView timeLineLikeText;
+    private Button timeLineWriteBtn;
+    private Button timeLineCommentBtn;
+    private TextView timeLineCommentText;
 
     public static PageFragment newInstance(int page) {
         Bundle args = new Bundle();
@@ -35,15 +47,35 @@ public class PageFragment extends Fragment {
         for (int i = 0; i < 10; i++) {
             item[i] = new RecyclerItem(R.drawable.i2);
         }
+
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = null;
+
         switch (pageNum) {
             case 1:
                 view = inflater.inflate(R.layout.fragment_timeline, container, false);
+                timeLineLikeBtn = (ImageButton) view.findViewById(R.id.timeLineLikeBtn);
+                timeLineLikeText = (TextView) view.findViewById(R.id.timeLineLikeText);
+                timeLineWriteBtn = (Button) view.findViewById(R.id.timeLineWriteBtn);
+                timeLineCommentBtn = (Button) view.findViewById(R.id.timeLineCommentBtn);
+                timeLineCommentText = (TextView) view.findViewById(R.id.timeLineCommentText);
+
+                timeLineLikeBtn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if(i==0) {
+                            timeLineLikeBtn.setBackgroundDrawable(getResources().getDrawable(R.drawable.like_push));
+                            i++;
+                        } else if(i==1) {
+                            timeLineLikeBtn.setBackgroundDrawable(getResources().getDrawable(R.drawable.like));
+                            i=0;
+                        }
+                    }
+                });
                 break;
             case 2:
                 view = inflater.inflate(R.layout.fragment_mybook, container, false);
