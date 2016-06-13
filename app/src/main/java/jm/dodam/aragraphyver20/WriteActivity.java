0995fb1ba429c9f7420a1f8027b584269d39a82a
gkskzh100 <a22569447@gmail.com>
2016-06-14 01:29:06 +0900

    Kim ver 2.4
    WriteActivity

--- app/src/main/java/jm/dodam/aragraphyver20/WriteActivity.java
+++ app/src/main/java/jm/dodam/aragraphyver20/WriteActivity.java
@@ -1,13 +1,31 @@
 package jm.dodam.aragraphyver20;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
 
 public class WriteActivity extends Activity {
+
+    private ImageButton writeBackBtn;
+    private ImageButton writeUploadBtn;
+    private EditText writeCalEdit;
+    private EditText writeTalkEdit;
+    private ImageButton writeBackgroundBtn;
+    private EditText writeHashTagEdit;
+
+    private LinearLayout writeCalBackLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,10 +33,58 @@ public class WriteActivity extends Activity {
         setContentView(R.layout.activity_write);
 
         init();
+
+        writeCalEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                //입력하기 전에
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //입력되는 텍스트에 변화가 있을때
+                Typeface type = Typeface.createFromAsset(getAssets(),"fonts/dxSenal.ttf");
+                writeCalEdit.setTypeface(type);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                //입력이 끝났을때
+                String string = s.toString();
+                if(string.length()==0) {
+                    int backColor = getResources().getColor(R.color.colorBackground);
+                    writeCalBackLayout.setBackgroundColor(backColor);
+                    writeCalEdit.setTypeface(Typeface.SERIF);
+                } else {
+                    writeCalBackLayout.setBackgroundColor(Color.TRANSPARENT);
+                }
+
+            }
+        });
     }
 
     private void init() {
+        writeBackBtn = (ImageButton) findViewById(R.id.writeBackBtn);
+        writeUploadBtn = (ImageButton) findViewById(R.id.writeUploadBtn);
+        writeCalEdit = (EditText) findViewById(R.id.writeCalEdit);
+        writeTalkEdit = (EditText) findViewById(R.id.writeTalkEdit);
+        writeBackgroundBtn = (ImageButton) findViewById(R.id.writeBackgroundBtn);
+        writeHashTagEdit = (EditText) findViewById(R.id.writeHashTagEdit);
+        writeCalBackLayout = (LinearLayout) findViewById(R.id.writeCalBackLayout);
         setHideStatusBar();
+        setListener();
+    }
+
+    private void setListener() {
+
+        writeBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
     }
 
     private void setHideStatusBar() {
